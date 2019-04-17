02552d360ab00edec71b8bc67a84504e57c6f491
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2019-04-17 15:48:03 -0300

    removendo namespace

--- clientes.h
+++ clientes.h
@@ -5,8 +5,6 @@
 #include <string>
 #include <list>
 
-using namespace std; // ranca isso aqui
-
 typedef struct cliente
 {
   int codigo;
@@ -15,11 +13,11 @@ typedef struct cliente
 } Cliente;
 
 char printMenuCliente();
-int gerarCodigo(list<Cliente>& listCliente);
-void adicionarCliente(list<Cliente>& listCliente);
-void listarCliente(list<Cliente>& listCliente);
-void alterarCliente(list<Cliente>& listCliente);
-void excluirCliente(list<Cliente>& listCliente);
-void visualizarCliente(list<Cliente>& listCliente);
+int gerarCodigo(std::list<Cliente>& listCliente);
+void adicionarCliente(std::list<Cliente>& listCliente);
+void listarCliente(std::list<Cliente>& listCliente);
+void alterarCliente(std::list<Cliente>& listCliente);
+void excluirCliente(std::list<Cliente>& listCliente);
+void visualizarCliente(std::list<Cliente>& listCliente);
 
 #endif
